6e7513731ffc89530b0ca415232d16333711fe9e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-05 14:15:26 +0100

    toc used
    The toc library is used to automatically generate the table of
    contents.

--- js/presentation.js
+++ js/presentation.js
@@ -1,5 +1,13 @@
 (function($, undefined){
     $(function(){
 	$.deck(".slide");
+
+	$("#toc").toc({
+	    'selectors' : 'h2',
+	    'container' : 'body',
+	    'smoothScrolling': false,
+	    'prefix': 'toc',
+	    'highlightOnScroll': false
+	});
     });
 })( jQuery );
