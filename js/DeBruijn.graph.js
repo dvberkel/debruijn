babe3103f65918bee9ca349372b5346c8a112cc4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-09 14:55:53 +0100

    GraphView uses model
    The DeBruijn.GraphView uses the model to generate all the combinations.
    Besides that it also links them up in the correct manner.

--- js/DeBruijn.graph.js
+++ js/DeBruijn.graph.js
@@ -1,23 +1,86 @@
 (function($, _, Backbone, d3, bruijn, undefined){
+    var on = function(word){
+	return {
+	    pipe: function(letter){
+		result = word.slice(0);
+		result.shift();
+		result.push(letter);
+		return result;
+	    }
+	};
+    };
 
+    var does = function(element) {
+	if (element instanceof Array) {
+	    return {
+		equal: function(target) {
+		    if (target instanceof Array) {
+			if (element.length == target.length) {
+			    for (var i = 0; i < element.length; i++) {
+				if (!does(element[i]).equal(target[i])) {
+				    return false;
+				}
+			    }
+			    return true;
+			}
+		    }
+		    return false;
+		}
+	    };
+	} else {
+	    return {
+		equal: function(target){ return element == target }
+	    };
+	}
+    };
+
+    var within = function(array){
+	return {
+	    locate: function(target) {
+		for (var i = 0; i < array.length; i++) {
+		    var element = array[i];
+		    if (does(element).equal(target)) {
+			return i;
+		    }
+		}
+		return -1;
+	    }
+	};
+    };
+    
     bruijn.GraphView = Backbone.View.extend({
 	initialize: function(){
 	    this.render();
 	},
 	
 	render: function(){
-	    var options = this.options;
+	    var view = this;
+	    var model = view.model;
+	    var alphabet = model.get("alphabet").slice(0, model.get("k"));
+	    var n = model.get("n");
+	    
+	    var combinations = [];
+	    bruijn.Combinatorics.allCombinations(alphabet, n - 1, function(word){
+		combinations.push(word);
+	    });
+	    
+	    var nodes = _.map(combinations, function(combination){
+		return {word: combination.join("")};
+	    });
+	    var links = [], searcher = within(combinations);
+	    _.each(combinations, function(combination, sourceIndex){
+		var combi = on(combination);
+		_.each(alphabet, function(letter){
+		    var target = combi.pipe(letter);
+		    var targetIndex = searcher.locate(target);
+		    links.push({source: sourceIndex, target: targetIndex, letter: letter});
+		});
+	    });
+
+	    var options = view.options;
 	    var svg = d3.select("#" + $(this.el).attr('id')).append("svg")
 	        .attr("width", options.width)
 	        .attr("height", options.height);
-	    
-	    var nodes = [{word: "00"}, {word: "01"}, {word: "10"},  {word: "11"}];
-	    var links = [
-		{source: 0, target: 1, letter: "0"},
-		{source: 0, target: 2, letter: "0"},
-		{source: 0, target: 3, letter: "0"},
-		{source: 2, target: 3, letter: "1"}
-	    ];
 
 	    var force = d3.layout.force().charge(-120).linkDistance(30).size([options.width, options.height])
 	        .nodes(nodes)
