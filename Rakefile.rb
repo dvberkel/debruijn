df2cc72350e7d86d79718c47ebc1711e6b0f1a44
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-14 15:32:10 +0100

    Test task
    A test task is created which runs rspec on the spec directory.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env rake
-require "bundler/gem_tasks"
+
+require "rubygems"
+require "bundler/setup"
+
+require 'rspec/core/rake_task'
+
+task :default => [:test]
+
+desc "Run rspec on the spec directory"
+RSpec::Core::RakeTask.new('test') do |task|
+  task.rspec_opts = ['-I lib -I lib/debruijn']
+  task.pattern = 'spec/**/*_spec.rb'
+end
