b1f069b044cc961fde1f34cfcaebc94f3eb49d3a
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-15 15:46:25 +0100

    Words test
    the words shouldn not only return a correct number, but they should
    also be correct.

--- spec/wordgenerator_spec.rb
+++ spec/wordgenerator_spec.rb
@@ -1,4 +1,5 @@
 require "word_generator"
+require "set"
 
 describe "Words" do
   [ 
@@ -10,13 +11,34 @@ describe "Words" do
         @words = Words.over(alphabet)
       end
       
-      it "should generate all words over #{alphabet} of length #{length}" do
+      it "should count all words over #{alphabet} of length #{length}" do
         count = 0;
         
         @words.of_length(length) {|word| count += 1}
         
         count.should == alphabet.length ** length
       end
+    end
+  end
+  
+  [
+   [["a"], 0, [Word.new()].to_set]
+   [["a"], 1, [Word.new("a")].to_set]
+   [["a"], 2, [Word.new("a", "a")].to_set]
+   [["a"], 3, [Word.new("a", "a", "a")].to_set]
+   [["a", "b"], 0, [Word.new()].to_set]
+   [["a", "b"], 1, [Word.new("a"), Word.new("b")].to_set]
+   [["a", "b"], 2, [Word.new("a", "a"), Word.new("a", "b"), Word.new("b", "a"), Word.new("b", "b")].to_set]
+  ].each do |fixture|
+    alphabet = fixture[0]
+    length = fixture[1]
+    expectedSet = fixture[2]
+    it "should generate all the words over #{alphabet} of length #{length}" do
+      words = []
+      Words.over(alphabet).of_length(length) { |word|
+        words << word
+      }
+      words.to_set.should == expectedSet
     end
   end
 end
