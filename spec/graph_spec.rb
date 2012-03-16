0684e39a7f82f5152c958d830b147680fe2aef29
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-16 09:04:02 +0100

    [FIX] vertices spec
    the vertices fixture was missing due to a copy error.

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -4,6 +4,7 @@ require "word"
 describe "graph" do
   describe "vertices" do
     [
+     ["a"], ["a", "b"], ["a", "b", "c"], ["a", "b", "c", "d"]
     ].each do |alphabet|
       it "should be able to add #{alphabet.length} vertices" do
         count, graph = 0, createGraph(alphabet)
