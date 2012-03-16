3d9f3150dce4608653bcd66fcf0f81df16bf8881
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-16 09:02:43 +0100

    Edges added
    It is possible to add the edges.

--- lib/debruijn/graph.rb
+++ lib/debruijn/graph.rb
@@ -1,6 +1,7 @@
 class Graph
   def initialize
     @vertices = []
+    @edges = []
   end
 
   def addVertex(vertex)
@@ -11,5 +12,38 @@ class Graph
     @vertices.each { |vertex|
       yield vertex
     }
+  end
+
+  def addEdgeFrom(source)
+    EdgeAdder.new(self, source)
+  end
+
+  def addEdge(source, sink)
+    @edges << Edge.new(source, sink)
+  end
+
+  def allEdges
+    @edges.each { |edge|
+      yield edge
+    }
+  end
+end
+
+class Edge
+  def initialize(source, sink)
+    @source = source
+    @sink = sink
+  end
+end
+
+class EdgeAdder
+  def initialize(graph, source)
+    @graph = graph
+    @source = source
+  end
+
+  def to(sink)
+    @graph.addEdge(@source, sink)
+    self
   end
 end
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -2,26 +2,47 @@ require "graph"
 require "word"
 
 describe "graph" do
-  [
-   ["a"],
-   ["a", "b"],
-   ["a", "b", "c"],
-   ["a", "b", "c", "d"],
-  ].each do |alphabet|
-    it "should be able to add #{alphabet.length} vertices" do
-      count, graph = 0, createGraph(alphabet)
+  describe "vertices" do
+    [
+    ].each do |alphabet|
+      it "should be able to add #{alphabet.length} vertices" do
+        count, graph = 0, createGraph(alphabet)
+        
+        graph.allVertices {count += 1}
+        
+        count.should == alphabet.length
+      end
+    end
+  end
+
+  describe "edges" do
+    [
+     [["a"], [["a", "a"]]],
+     [["a", "b"], [["a", "b"], ["b", "a"]]],
+     [["a", "b", "c"], [["a", "b"], ["b", "c"], ["c", "a"]]],
+     [["a", "b", "c", "d"],[["a", "b"], ["b", "c"], ["c", "d"], ["d", "a"]]],
+    ].each do |fixture|
+      alphabet = fixture[0]
+      edges = fixture[1]
       
-      graph.allVertices {count += 1}
-      
-      count.should == alphabet.length
+      it "should be able to add #{edges.length} edges" do
+        count, graph = 0, createGraph(alphabet, edges)
+
+        graph.allEdges {count += 1}
+
+        count.should == edges.length
+      end
     end
   end
 end
 
-def createGraph(alphabet)
+def createGraph(alphabet, edges=[])
   graph = Graph.new
   alphabet.each { |letter|
     graph.addVertex(Word.new(letter))
+  }
+  edges.each { |edge|
+    graph.addEdgeFrom(Word.new(edge[0])).to(Word.new(edge[1]))
   }
   graph
 end
