e25b24d503aa530cca071c32665283b1cc70b4ad
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-05-06 20:26:25 +0200

    Graph edges
    The graph edges are added the the project.

--- debruijn/graph/graph.py
+++ debruijn/graph/graph.py
@@ -1,6 +1,7 @@
 class Graph:
     def __init__(self):
         self.vertices = []
+        self.edges = []
 
     def addVertex(self, word):
         self.vertices.append(word)
@@ -8,3 +9,40 @@ class Graph:
     def allVertices(self):
         for word in self.vertices:
             yield word
+    
+    def addEdgeFrom(self, vertex):
+        return EdgeAdder(self, vertex)
+    
+    def addEdge(self, edge):
+        self.edges.append(edge)
+
+    def allEdges(self):
+        for edge in self.edges:
+            yield edge
+
+class EdgeAdder:
+    def __init__(self, graph, vertex):
+        self.graph = graph
+        self.vertex = vertex
+
+    def to(self, vertex):
+        self.graph.addEdge(Edge(self.vertex, vertex))
+
+class Edge:
+    def __init__(self, source, sink):
+        self.source = source
+        self.sink = sink
+        
+    def __eq__(self, other):
+        if (isinstance(other, Edge)):
+            return self.source == other.source and self.sink == other.sink
+        return false
+
+    def __hash__(self):
+        return hash(self.source) * hash(self.sink)
+
+    def __repr__(self):
+        return "Edge({0}, {1})".format(self.source, self.sink)
+
+    def __str__(self):
+        return "{{ {0} -> {1}  }}".format(self.source, self.sink)
--- debruijn/graph/test/graph.py
+++ debruijn/graph/test/graph.py
@@ -1,13 +1,17 @@
 import unittest
 
 from debruijn.graph.graph import Graph
+from debruijn.graph.graph import Edge
 from debruijn.combinatorics.word import Word
 
-def createGraph(*alphabet):
+def createGraph(edges = [], *alphabet):
     graph = Graph()
     
     for letter in alphabet:
         graph.addVertex(Word(letter))
+
+    for edge in edges:
+        graph.addEdgeFrom(Word(edge[0])).to(Word(edge[1]))
 
     return graph
         
@@ -15,24 +19,40 @@ def createGraph(*alphabet):
 class testGraph(unittest.TestCase):
     def setUp(self):
         self.alphabet = ["a", "b", "c"]
+        self.edges = [("a", "b"), ("b", "c"), ("c", "a")]
+        self.graph = createGraph(self.edges, *self.alphabet)
 
     def testVertexCount(self):
-        graph = createGraph(*(self.alphabet))
         count = 0
 
-        for vertex in graph.allVertices():
+        for vertex in self.graph.allVertices():
             count = count + 1
 
         self.assertEquals(len(self.alphabet), count)
             
     def testVertices(self):
-        graph = createGraph(*(self.alphabet))
-
-        actual = [v for v in graph.allVertices()]
+        actual = [v for v in self.graph.allVertices()]
 
         for word in map((lambda v: Word(v)), self.alphabet):
             self.assertTrue(word in actual)
-            
+
+    def testEdgeCount(self):
+        count = 0
+
+        for edge in self.graph.allEdges():
+            count = count + 1
+
+        self.assertEquals(len(self.edges), count)
+
+    def testEdgeEquality(self):
+        self.assertTrue(Edge(Word("a"), Word("b")), Edge(Word("a"), Word("b")))
+
+    def testEdges(self):
+        actual = [e for e in self.graph.allEdges()]
+
+        for edge in map((lambda e: Edge(Word(e[0]), Word(e[1]))), self.edges):
+            self.assertTrue(edge in actual)
+        
 
 if __name__ == '__main__':
     unittest.main()
