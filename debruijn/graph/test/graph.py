1628e24040326ed2e6811f3e43433c33335c6896
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-05-06 19:45:23 +0200

    Graph skeleton
    A (test) skeleton for a graph is added the project.

---
+++ debruijn/graph/test/graph.py
@@ -0,0 +1,7 @@
+import unittest
+
+class testGraph(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
--- debruijn/test/test_all.py
+++ debruijn/test/test_all.py
@@ -2,11 +2,12 @@ import unittest
 
 from debruijn.combinatorics.test.word import testWord
 from debruijn.combinatorics.test.wordgenerator import testWordGenerator
+from debruijn.graph.test.graph import testGraph
 
 class EvaluateSuite(unittest.TestSuite):
     def __init__(self):
         unittest.TestSuite.__init__(self)
-        for clazz in [testWord, testWordGenerator]:
+        for clazz in [testWord, testWordGenerator, testGraph]:
             self.addTest(unittest.makeSuite(clazz))
 
 if __name__ == '__main__':
